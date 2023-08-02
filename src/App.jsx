fbea233d7224395d053e20c91436b9e4f27981f2
devguty22 <jmgutierreze@bch.bancodechile.cl>
2023-08-01 20:26:10 -0500

    UPDATE LOCALSTORAGE JSON

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import TodoCreate from "./components/TodoCreate";
 import TodoList from "./components/TodoList";
 import TodoComputed from "./components/TodoComputed";
 import TodoFilter from "./components/TodoFilter";
-import { useState } from "react";
-const initialStateTodos = [
-  { id: 1, title: "Complete Online Javascript", completed: true },
-  { id: 2, title: "Go to the gym", completed: false },
-  { id: 3, title: "pick up groceries", completed: false },
-  { id: 4, title: "Read for 1 hour", completed: false },
-  { id: 5, title: "Complete Todo App on Frontend Mentor", completed: false },
-];
+import { useEffect, useState } from "react";
+// const initialStateTodos = [
+//   { id: 1, title: "Complete Online Javascript", completed: true },
+//   { id: 2, title: "Go to the gym", completed: false },
+//   { id: 3, title: "pick up groceries", completed: false },
+//   { id: 4, title: "Read for 1 hour", completed: false },
+//   { id: 5, title: "Complete Todo App on Frontend Mentor", completed: false },
+// ];
+
+const initialStateTodos = JSON.parse(localStorage.getItem("todos")) || []
 
 const App = () => {
   const [todos, setTodos] = useState(initialStateTodos);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   const createTodo = (title) => {
     const newTodo = {
@@ -26,44 +32,62 @@ const App = () => {
   };
 
   const removeButtonCheck = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id))
-  }
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   const updateButtonCheck = (id) => {
-    setTodos(todos.map(todo => todo.id === id ? { ...todo, completed: !todo.completed } : todo))
-  }
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo,
+      ),
+    );
+  };
 
-  const countItemsLeftTodo = todos.filter((todo) => !todo.completed).length
+  const countItemsLeftTodo = todos.filter((todo) => !todo.completed).length;
 
   const clearItemsCompletedTodo = () => {
-    setTodos(todos.filter((todo) => !todo.completed))
-  }
+    setTodos(todos.filter((todo) => !todo.completed));
+  };
 
-  const [filter, setFilter] = useState("all")
-  const changeFilter = (filter) => setFilter(filter)
+  const [filter, setFilter] = useState("all");
+  const changeFilter = (filter) => setFilter(filter);
   const filteredTodos = () => {
     switch (filter) {
-      case "all": return todos;
-      case "active": return todos.filter((todo) => !todo.completed);
-      case "completed": return todos.filter((todo) => todo.completed);
-      default: return todos;
+      case "all":
+        return todos;
+      case "active":
+        return todos.filter((todo) => !todo.completed);
+      case "completed":
+        return todos.filter((todo) => todo.completed);
+      default:
+        return todos;
     }
-  }
+  };
 
   return (
     <div
       className="min-h-screen
-      dark:bg-gray-900
-      bg-gray-300 
+      bg-gray-300
       bg-[url('./assets/images/bg-mobile-light.jpg')] 
-      dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] 
-      bg-contain bg-no-repeat"
+      bg-contain 
+      bg-no-repeat 
+      dark:bg-gray-900 
+      dark:bg-[url('./assets/images/bg-mobile-dark.jpg')]
+      md:bg-[url('./assets/images/bg-desktop-light.jpg')]
+      md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')]"
     >
       <TodoHeader />
-      <main className="container mx-auto mt-8 px-4">
+      <main className="container mx-auto mt-8 px-4 md:max-w-xl">
         <TodoCreate createTodo={createTodo} />
-        <TodoList todos={filteredTodos()} removeButtonCheck={removeButtonCheck} updateButtonCheck={updateButtonCheck} />
-        <TodoComputed countItemsLeftTodo={countItemsLeftTodo} clearItemsCompletedTodo={clearItemsCompletedTodo} />
+        <TodoList
+          todos={filteredTodos()}
+          removeButtonCheck={removeButtonCheck}
+          updateButtonCheck={updateButtonCheck}
+        />
+        <TodoComputed
+          countItemsLeftTodo={countItemsLeftTodo}
+          clearItemsCompletedTodo={clearItemsCompletedTodo}
+        />
         <TodoFilter changeFilter={changeFilter} filter={filter} />
       </main>
       <footer className="mt-8 px-4 py-4 text-center text-gray-500 dark:text-gray-400">
--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -8,17 +8,15 @@ const TodoHeader = () => {
   useEffect(() => {
     if (darkMode) {
       document.documentElement.classList.add("dark")
-      console.log("mode dark")
       localStorage.theme = "dark"
     } else {
       document.documentElement.classList.remove("dark")
-      console.log("mode light")
       localStorage.theme = "light"
     }
   }, [darkMode])
 
   return (
-    <header className="container mx-auto px-4 pt-8">
+    <header className="container mx-auto px-4 pt-8 md:max-w-xl">
       <div className="flex justify-between">
         <h1 className="text-2xl font-semibold uppercase tracking-[0.3em] text-white">
           Todo
